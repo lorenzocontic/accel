48d8528d70e302d9610fa693cfa0df0a8d5b0c7f
lorenzocontic <lorenzocontic@proton.me>
2026-07-11 18:27:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
-""" Local URL shortener """
+from validator import valid
+from shortener import generate
+from storage import save, all_links
+from sample_links import LINKS
 
-print("Project: url-shortener-mock")
+print("=" * 35)
+print("LOCAL URL SHORTENER")
+print("=" * 35)
+
+for url in LINKS:
+
+    if valid(url):
+
+        alias = generate(url)
+
+        save(alias, url)
+
+for alias, url in all_links().items():
+
+    print(url)
+    print("→", alias)
+    print()
+
+print(f"Stored URLs: {len(all_links())}")
